24b133ebce0201d32f82d8f4bdd188ac52bb94c3
Raphael Almeida <phael92br@gmail.com>
2024-12-15 14:30:58 -0300

    adição do projeto de pipeline no portfolio

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ AOS.init({
     duration: 1200, // Animation duration
     once: true, // Whether animation should happen only once
   });
+
+
+// Path to the sound file
+const soundEffect = new Audio('./sounds/laser_shooting.mp3');
+
+
+// Add event listeners to all buttons with the class 'sound-button'
+document.querySelectorAll('.sound-button').forEach(button => {
+    button.addEventListener('click', () => {
+        // Play the sound effect
+        soundEffect.currentTime = 0; // Reset sound to start for overlapping clicks
+        soundEffect.play();
+    });
+});
